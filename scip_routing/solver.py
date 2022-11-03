f3e227fea2969b21c36afbe5197da461d946c3da
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2022-11-03 09:00:18 +0100

    Fix functionality of verbose parameter

--- scip_routing/solver.py
+++ scip_routing/solver.py
@@ -1,5 +1,3 @@
-from time import time
-
 from scip_routing.pricing import Pricer
 import pyscipopt as scip
 
@@ -10,25 +8,24 @@ class VRPTWSolver:
         self.end_depot = instance.n_customers + 1
         self.customers = instance.customers
         self.pricer = Pricer(graph, instance, distance_fn=distance_fn)
+        self.verbose = verbose
         self.rmp = self.init_rmp()
         init_cons = list(self.rmp.getConss())
         self.pricer.set_init_cons(init_cons)
         self.rmp.includePricer(self.pricer, "LabelingPricer", "")
-        self.verbose = verbose
         self.obj = float("inf")
 
     def init_rmp(self):
         rmp = scip.Model()
-        vars = []
         for customer in self.customers:
             var_name = f"{self.start_depot}-{customer}-{self.end_depot}"
             cost = self.pricer.graph[self.start_depot][customer]["distance"] + \
                    self.pricer.graph[customer][self.end_depot]["distance"]
             var = rmp.addVar(obj=cost, name=var_name)
-            vars.append(var)
-
             rmp.addCons(var == 1, separate=False, modifiable=True)
         rmp.setMinimize()
+        if not self.verbose:
+            rmp.hideOutput()
         return rmp
 
     def solve(self):
