e6497973b8157dfd93793907abe6e7d36c74175f
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2022-12-05 18:19:52 +0200

    Test equivalence of python and rust solvers

--- tests/test_colgen.py
+++ tests/test_colgen.py
@@ -7,14 +7,19 @@ from scip_routing.utils import minify_instance, instance_graph
 def test_finds_optimal():
     instance, sol = cvrplib.download('R101', solution=True)
 
-    instance = minify_instance(instance, 20)
+    instance = minify_instance(instance, 50)
 
     graph = instance_graph(instance)
 
-    solver = VRPTWSolver(graph=graph,
+    py_solver = VRPTWSolver(graph=graph,
                          instance=instance,
-                         verbose=True)
+                         verbosity=1,
+                         pricing_strategy="py")
+    py_solver.solve()
+    rust_solver = VRPTWSolver(graph=graph,
+                         instance=instance,
+                         verbosity=1,
+                         pricing_strategy="rust")
+    rust_solver.solve()
 
-    solver.solve()
-
-    assert solver.rmp.getObjVal() == 507
+    assert py_solver.rmp.getObjVal() == rust_solver.rmp.getObjVal() == 1031
